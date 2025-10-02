9516cafd994bababc8650667b7cffc1da28a1878
Dwait Pandhi <dwait.pandhi@gmail.com>
2025-10-02 17:47:07 -0400

    added animated favicon

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+var current = 0;
+var icons = ['boba1.png', 'boba2.png'];
+setInterval(function () {
+    var icon = (++current % icons.length);
+    var url = icons[icon];
+    var favicon = document.getElementById('favicon');
+    if (favicon) {
+        favicon.href = url;
+    }
+}, 50);
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('alphabetButton');
     let currentCharCode = 97;
